5a11165f69832f1248850c17bba760f4f04f71d5
karan jagota <karan.02031993@gmail.com>
2020-10-26 23:10:00 +0530

    add snapshots

--- screens/home.js
+++ screens/home.js
@@ -8,8 +8,8 @@ export default function Home({ navigation }) {
   const data = [
     {title: "About ReactNative",source:require("../assets/raw_images/reactlogo.png"),info:"Learn About the basics of React Native, Why you should use react Native .. ", id:1},
     {title: "Learn React Native (Step By Step)",source:require("../assets/raw_images/ab.png"),info:"Learn About the nuts and bolts of react native components .. How to create components and more .", id:2},
-    {title: "Find React Jobs",source:require("../assets/raw_images/jobs.png"),info:"Learn how to create applications with ReactNative .. How to create mobile apps and much more .", id:3},
-    {title: "Generate Bundle & Deploy",source:require("../assets/raw_images/dply.png"),info:"Find Jobs and Connect with the React Community ..Get to know people with similar interests .", id:4}
+    {title: "Find React Jobs",source:require("../assets/raw_images/jobs.png"),info:"Find New Everyday React Jobs and Apply Online Instantaneously.", id:3},
+    {title: "Generate Bundle & Deploy",source:require("../assets/raw_images/dply.png"),info:"Learn How to deploy app on Google play store and Apple App Store", id:4}
   ]
   
     const presshandler = (id)=>{
